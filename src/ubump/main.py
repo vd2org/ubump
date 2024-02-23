ee8bd7a15f0fec01d8550b4a48e00cc488c59d6e
root <33198864+vd2org@users.noreply.github.com>
2024-02-23 17:11:57 +0000

    Bump to v0.1.4

--- src/ubump/main.py
+++ src/ubump/main.py
@@ -18,7 +18,7 @@ from packaging.version import parse
 from tomlkit.exceptions import TOMLKitError
 
 NAME = "ubump"
-VERSION = "v0.1.3"
+VERSION = "v0.1.4"
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
 logger = logging.getLogger(NAME)
