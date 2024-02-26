3a768bf0816e841bf271b445d58e535c77db325e
vd <33198864+vd2org@users.noreply.github.com>
2024-02-26 15:21:57 +0400

    Use CI.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,10 +1,12 @@
+# Copyright (C) 2024 by Vd <ubump@vd2.org>.
+# This file is part of ubump project.
+# ubump is released under the MIT License (see LICENSE).
+
+
 from unittest import mock
 
 from ubump import main
 
 
 def test_config():
-    with mock.patch("builtins.open", mock.mock_open(read_data=b'file content'), side_effect=FileNotFoundError):
-        config = main.Config()
-
-        print(config.file)
+    pass
