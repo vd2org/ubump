ae54efe12dc21cc028572eab92c74b0e436e7632
vd <33198864+vd2org@users.noreply.github.com>
2024-03-03 23:50:16 +0400

    Bump to v0.1.14

--- src/ubump/main.py
+++ src/ubump/main.py
@@ -19,7 +19,7 @@ from packaging.version import parse
 from tomlkit.exceptions import TOMLKitError
 
 NAME = "ubump"
-VERSION = "v0.1.13"
+VERSION = "v0.1.14"
 
 DEFAULT_TEMPLATE = "${major}.${minor}.${patch}"
 
